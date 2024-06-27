08e928d09891d5dfde2289f5fb0fe3df9e37a6d0
Megan <megan.nyakina@student.moringaschool.com>
2024-06-27 16:53:26 +0300

    adds looping code lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+let names =["Quiin", "Nelly" ," Clark"];
+
+ let eventName = 'birthday';
+
+function writeCards(names,eventName) {
+
+let cards=[];
+
+
+for (let i=0; i<names.length;i++) {
+
+   let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+
+   cards.push(message);
+}
+
+   return cards;
+}
+
+
+countDown(10);
+
+function  countDown(number){
+
+    if (number < 0)
+        { console.log("Enter a positive integer.");
+            return;
+        }
+
+        for (let i = number ; i >= 0; i--)
+
+            {console.log(i)}
+
+}
+
+
